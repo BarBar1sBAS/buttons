cbfd08a52d23a58aa776be6defa48d1f0d876eb1
Борис Басов <b.basov@oshara.CEK.local>
2025-08-14 13:14:53 +0300

    Initial commit: Button component system with SCSS and BEM

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      clean: true,
+    },
+    devServer: {
+      static: './dist',
+      hot: true,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    ['autoprefixer'],
+                  ],
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                api: 'modern',
+                sassOptions: {
+                  silenceDeprecations: ['legacy-js-api'],
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name].[hash][ext]',
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name].[hash][ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
+      ...(isProduction ? [
+        new MiniCssExtractPlugin({
+          filename: '[name].[contenthash].css',
+        }),
+      ] : []),
+    ],
+    resolve: {
+      extensions: ['.js', '.scss'],
+    },
+  };
+};
